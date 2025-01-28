b6e48c1e67d74a17d4586512b85cadc04c738216
Alifah Zuhrah <alifah.zuhrah@gmail.com>
2025-01-28 21:25:24 +0700

    First commit

---
+++ testing_deploy.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+import joblib
+ 
+# Inisialisasi aplikasi Flask
+app = Flask(__name__)
+ 
+# Memuat model yang telah disimpan
+joblib_model = joblib.load('gbr_model.joblib') # Pastikan path file sesuai dengan penyimpanan Anda
+ 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.json['data']  # Mengambil data dari request JSON
+    prediction = joblib_model.predict(data)  # Melakukan prediksi (harus dalam bentuk 2D array)
+    return jsonify({'prediction': prediction.tolist()})
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+# command: curl -X POST http://127.0.0.1:5000/predict -H "Content-Type: application/json" -d @data.json

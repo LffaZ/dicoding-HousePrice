2af579adff9a7268f496206c11d527ec5e6db1e0
Alifah Zuhrah <alifah.zuhrah@gmail.com>
2025-01-28 21:51:15 +0700

    Fixed API error issue

--- testing_deploy.py
+++ testing_deploy.py
@@ -1,19 +1,27 @@
 from flask import Flask, request, jsonify
 import joblib
- 
-# Inisialisasi aplikasi Flask
+
 app = Flask(__name__)
- 
-# Memuat model yang telah disimpan
 joblib_model = joblib.load('gbr_model.joblib') # Pastikan path file sesuai dengan penyimpanan Anda
- 
+
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json['data']  # Mengambil data dari request JSON
-    prediction = joblib_model.predict(data)  # Melakukan prediksi (harus dalam bentuk 2D array)
-    return jsonify({'prediction': prediction.tolist()})
- 
+    data = request.json.get('data', None)  # Mengambil data dari request JSON
+    
+    if not data:
+        return jsonify({"error": "Data not found in request"}), 400
+    
+    # Validasi apakah data berupa list of lists
+    if not isinstance(data, list) or not all(isinstance(i, list) for i in data):
+        return jsonify({"error": "Data should be a 2D array"}), 400
+
+    try:
+        prediction = joblib_model.predict(data)  # Melakukan prediksi
+        return jsonify({'prediction': prediction.tolist()})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
-    
-# command: curl -X POST http://127.0.0.1:5000/predict -H "Content-Type: application/json" -d @data.json
+
+# command: Invoke-RestMethod -Uri "http://127.0.0.1:5000/predict" -Method Post -Headers @{ "Content-Type" = "application/json" } -Body (Get-Content -Raw -Path "data.json")
